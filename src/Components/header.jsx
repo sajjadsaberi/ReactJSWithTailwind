f9f64b09ad4ab70e6db908e5cc151811c5dde56b
Sajjad <sajjad.saberi1996@gmail.com>
2023-12-23 14:14:08 +0330

    Create React APP with Tailwind CSS

---
+++ src/Components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Headers() {
+    return (
+        <h1 className="text-3xl font-bold mt-20 shadow-xl">
+        Hello world!
+        </h1>
+    );
+};
+
+export default Headers ;
